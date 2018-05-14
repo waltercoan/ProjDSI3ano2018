4b25490de54ae1b9ccb29e205c4eb89fabed5a22
Walter Silvestre Coan <walter.coan@gmail.com>
2018-05-14 18:58:54 -0300

    14/05/2018

---
+++ README.md
@@ -0,0 +1,4 @@
+Projeto exemplo DSI 2018
+
+Manual em:
+http://www.faltoupontoevirgula.com.br/index.php/desenvolvimento-utilizando-spring-boot/
---
+++ src/main/java/br/univille/projdsi3ano2018/ProjDsi3ano2018Application.java
@@ -0,0 +1,12 @@
+package br.univille.projdsi3ano2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjDsi3ano2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjDsi3ano2018Application.class, args);
+	}
+}
